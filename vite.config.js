206511847d9c0b6b90de9b5da917ba0eb9cc2d4f
dhru2233 <dhruv13122004@gmail.com>
2026-04-05 19:24:09 +0530

    fix: vercel base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/blinkit-ims/', 
+  base: '/',   // change this from '/blinkit-ims/' to '/'
 })
